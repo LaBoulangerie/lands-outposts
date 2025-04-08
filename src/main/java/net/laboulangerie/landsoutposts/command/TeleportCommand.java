d56fa30d8a6d73cb77ff559451b278c819409fee
Clem Fern <clemfern456@gmail.com>
2025-04-08 21:20:14 +0200

    working :)

--- src/main/java/net/laboulangerie/landsoutposts/command/TeleportCommand.java
+++ src/main/java/net/laboulangerie/landsoutposts/command/TeleportCommand.java
@@ -24,38 +24,28 @@ public class TeleportCommand {
     }
 
     public static final LiteralArgumentBuilder<CommandSourceStack> command(LandsOutposts landsOutposts) {
-        // not working ???
-        //　　　　　 　 ____
-        //　　　　　／＞　　 フ
-        //　　　　　| 　_　 _l
-        //　 　　　／` ミ＿xノ
-        //　　 　 /　　　   |
-        //　　　 /　 ヽ　　 ﾉ
-        //　 　 │　　|　|　|
-        //　／￣|　　 |　|　|
-        //　| (￣ヽ＿ヽ)__) __)
-        //  ＼二つ 
-        return Commands.literal("tp").then(Commands.argument("outpost", IntegerArgumentType.integer(1))) 
-        .requires(sender -> sender.getSender() instanceof Player)
-        .executes(ctx -> {
-            Player player = (Player) ctx.getSource().getSender();
-            LandPlayer landPlayer = landsOutposts.getLands().getLandPlayer(player.getUniqueId());
+        return Commands.literal("tp").requires(sender -> sender.getSender() instanceof Player)
+        .then(Commands.argument("outpost", IntegerArgumentType.integer(1))
+            .executes(ctx -> {
+                Player player = (Player) ctx.getSource().getSender();
+                LandPlayer landPlayer = landsOutposts.getLands().getLandPlayer(player.getUniqueId());
 
-            int outpost = ctx.getArgument("outpost", int.class);
-            try {
-                List<LandOutpost> outposts = landsOutposts.getLandPlayerOutposts(landPlayer);
-                LandOutpost landOutpost = outposts.get(outpost);
-                if (landOutpost != null) {
-                    player.teleport(landOutpost.getSpawn());
-                } else {
-                    //TODO: not found
+                int outpost = ctx.getArgument("outpost", int.class);
+                try {
+                    List<LandOutpost> outposts = landsOutposts.getLandPlayerOutposts(landPlayer);
+                    LandOutpost landOutpost = outposts.get(outpost);
+                    if (landOutpost != null) {
+                        player.teleport(landOutpost.getSpawn());
+                    } else {
+                        //TODO: not found
+                    }
+                } catch (SQLException e) {
+                    player.sendRichMessage(LandsOutposts.UNEXPECTED_EXCEPTION_MSG);
+                    e.printStackTrace();
                 }
-            } catch (SQLException e) {
-                player.sendRichMessage(LandsOutposts.UNEXPECTED_EXCEPTION_MSG);
-                e.printStackTrace();
-            }
 
-            return Command.SINGLE_SUCCESS;
-        });
+                return Command.SINGLE_SUCCESS;
+            })
+        );
     }
 }
